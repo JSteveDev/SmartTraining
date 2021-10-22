15bbd29e895163e9d6a4565df61bb725d153636a
Joslain Stevens <joslainstevens@gmail.com>
2021-10-22 11:18:54 +0200

    Add DetailHistoryActivity and set his layout.

--- app/src/main/java/com/example/smarttraining/Controllers/Fragments/HistoryFragment.java
+++ app/src/main/java/com/example/smarttraining/Controllers/Fragments/HistoryFragment.java
@@ -3,45 +3,44 @@ package com.example.smarttraining.Controllers.Fragments;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.smarttraining.Adapters.SportIconAdapter;
-import com.example.smarttraining.Adapters.SportItemAdapter;
 import com.example.smarttraining.R;
 
 import butterknife.BindView;
 
 public class HistoryFragment extends BaseFragment {
 
-	@BindView(R.id.fragment_sport_icon_recyclerView)
-	RecyclerView recyclerView;
+    @BindView(R.id.fragment_sport_icon_recyclerView)
+    RecyclerView recyclerView;
 
-	// --------------
-	// BASE METHODS
-	// --------------
+    // --------------
+    // BASE METHODS
+    // --------------
 
-	@Override
-	protected BaseFragment newInstance() {
-		return new SportsFragment();
-	}
+    @Override
+    protected BaseFragment newInstance() {
+        return new SportsFragment();
+    }
 
-	@Override
-	protected int getFragmentLayout() {
-		return R.layout.fragment_history;
-	}
+    @Override
+    protected int getFragmentLayout() {
+        return R.layout.fragment_history;
+    }
 
-	@Override
-	protected void configureDesign() {
-		this.configureRecyclerView();
-	}
+    @Override
+    protected void configureDesign() {
+        this.configureRecyclerView();
+    }
 
-	@Override
-	protected void updateDesign() {
-	}
+    @Override
+    protected void updateDesign() {
+    }
 
-	// --------------
-	// UPDATE UI
-	// --------------
+    // --------------
+    // UPDATE UI
+    // --------------
 
-	private void configureRecyclerView() {
-		this.recyclerView.setAdapter(new SportIconAdapter(getResources().getStringArray(R.array.sportNameList), getResources().getStringArray(R.array.sportIconList)));
-	}
+    private void configureRecyclerView() {
+        this.recyclerView.setAdapter(new SportIconAdapter(getResources().getStringArray(R.array.sportNameList), getResources().getStringArray(R.array.sportIconList)));
+    }
 
 }
