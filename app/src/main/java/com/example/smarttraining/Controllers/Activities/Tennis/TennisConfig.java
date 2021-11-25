09314bd6a9d47a23983421cfefbd384d004e6f9c
Stevens Joslain <joslainstevens@gmail.com>
2021-11-25 23:16:26 +0100

    Tennis Config UI Reworked

--- app/src/main/java/com/example/smarttraining/Controllers/Activities/Tennis/TennisConfig.java
+++ app/src/main/java/com/example/smarttraining/Controllers/Activities/Tennis/TennisConfig.java
@@ -4,12 +4,16 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.example.smarttraining.Controllers.Activities.Badminton.BadmintonConfig;
@@ -77,16 +81,20 @@ public class TennisConfig extends AppCompatActivity {
         joueurs_equipe2[0] = findViewById(R.id.tennis_config_eq2_j1);
         joueurs_equipe2[1] = findViewById(R.id.tennis_config_eq2_j2);
 
+        ViewGroup.LayoutParams layout = joueurs_equipe1[1].getLayoutParams();
+
         switch (game_mode_value){
             case 0: //SIMPLE MODE
-                joueurs_equipe1[1].getLayoutParams().height = 0;
-                joueurs_equipe2[1].getLayoutParams().height = 0;
+                layout.height = 0;
                 break;
             case 1: //DOUBLE MODE
-                joueurs_equipe1[1].getLayoutParams().height = WindowManager.LayoutParams.WRAP_CONTENT;
-                joueurs_equipe2[1].getLayoutParams().height = WindowManager.LayoutParams.WRAP_CONTENT;
+                layout.height = LinearLayout.LayoutParams.WRAP_CONTENT;
                 break;
         }
+
+        joueurs_equipe1[1].setLayoutParams(layout);
+        joueurs_equipe2[1].setLayoutParams(layout);
+
     }
 
     private void initialisation_view_values() {
@@ -124,23 +132,26 @@ public class TennisConfig extends AppCompatActivity {
 
     private void SetModejeu() {
 
+        ViewGroup.LayoutParams layout = joueurs_equipe1[1].getLayoutParams();
+
         switch (game_mode_value) {
             case 0: //SIMPLE MODE
                 game_mode_value = 1;
                 game_mode.setText(getResources().getString(R.string.double_mode));
-                joueurs_equipe1[1].getLayoutParams().height = WindowManager.LayoutParams.WRAP_CONTENT;
-                joueurs_equipe2[1].getLayoutParams().height = WindowManager.LayoutParams.WRAP_CONTENT;
+                layout.height = LinearLayout.LayoutParams.WRAP_CONTENT;
+
                 break;
             case 1: //DOUBLE MODE
                 game_mode_value = 0;
                 game_mode.setText(getResources().getString(R.string.simple_mode));
-                joueurs_equipe1[1].getLayoutParams().height = 0;
-                joueurs_equipe2[1].getLayoutParams().height = 0;
+                layout.height = 0;
                 break;
             default:
                 Toast.makeText(this, "default", Toast.LENGTH_SHORT).show();
         }
 
+        joueurs_equipe1[1].setLayoutParams(layout);
+        joueurs_equipe2[1].setLayoutParams(layout);
     }
 
     private void SetNbSet() {
